4590bf28070ee09feea1b60c0d52d4303e1718a0
libbypeterson <elibbypeterson@gmail.com>
2022-07-09 15:33:09 -0500

    -m "in part 3 but list is not displaying and sql query test not passing in part 2"

--- queries.sql
+++ queries.sql
@@ -4,7 +4,9 @@
 # VARCHAR name
 # VARCHAR skills
 -- Part 2: Test it with SQL
-
+SELECT name
+FROM employer
+WHERE location = "St. Louis City"
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,13 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
-public Employer() {}
+    public Employer() {
+    }
 
     @NotBlank
     @Size(min = 3, max = 75, message = "Location must be between 3 and 75 characters")
@@ -28,4 +33,8 @@ public Employer() {}
                 '}';
 
     }
+@OneToMany
+@JoinColumn(name = "employer")
+    private List<Job> jobs = new ArrayList<Job>();
+
 }
